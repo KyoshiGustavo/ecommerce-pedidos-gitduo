6548c4c6dfe4cce7e2854b9a0b341ba6223d94b2
GUSTAVO KYOSHI KODAMA PRAJO <gustavo.prajo2@senaisp.edu.br>
2026-09-01 20:48:51 -0300

    feat: cria esqueleto da classe Cliente

---
+++ src/main/java/com/senai/ecommerce/modelo/Cliente.java
@@ -0,0 +1,12 @@
+package com.senai.ecommerce.modelo;
+
+/**
+ * Representa o módulo de cadastro e gestão de clientes do E-commerce.
+ */
+public class Cliente {
+    private Long id;
+    private String nome;
+    private String cpf;
+
+    // Construtor e Métodos serão implementados nas próximas aulas
+}
